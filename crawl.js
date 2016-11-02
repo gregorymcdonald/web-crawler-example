4df9412e1529ba60952386965fb1e3607bc56be1
EpsilonF <greg.mcd.social@gmail.com>
2016-11-02 16:14:05 -0500

    Added crawl structure outline

--- crawl.js
+++ crawl.js
@@ -1,1 +1,62 @@
-console.log('Hello World!');
+// require(s)
+
+// Flag(s)
+const DEBUG = true;
+const CRAWL_TAG = "EXAMPLE";
+
+// Helper Function(s)
+var log;
+
+log = function log(message){
+	console.log(CRAWL_TAG + ": " + message);
+}
+
+// Crawl Required Values
+var rateLimitInMilliseconds = 1000;
+
+// Crawl Required Functions
+var getURLSeedlist;
+var extractData;
+var extractURLs;
+var crawl;
+
+getURLSeedlist = function getURLSeedlist() {
+	var url_seedlist = [];
+
+	// Load the URL seedlist
+	url_seedlist.push("www.google.com");
+	url_seedlist.push("www.amazon.com");
+
+	return url_seedlist;
+};
+
+extractData = function extractData(html) {
+	var result = {};
+
+	// Process the HTML and extract relevant data
+	result.url = "www.google.com";
+
+	return result;
+};
+
+extractURLs = function extractURLs(html) {
+	var url_list = [];
+
+	// Process the HTML and extract all URLs to visit later
+
+	return url_list;
+};
+
+crawl = function crawl(url_seedlist){
+	var debugStatusString = (DEBUG) ? "ON" : "OFF";
+	log("Crawl started. Debug mode " + debugStatusString);
+
+	if(DEBUG){
+		log("Seedlist: " + url_seedlist)
+	}
+
+	// Crawl the web!
+}
+
+// Start crawling the web
+crawl(getURLSeedlist());
